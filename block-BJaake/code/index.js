beaa3cd7dfb0feacb5b3190f4cb5b1952e1bbce4
Akhil <akhilkum249@gmail.com>
2024-02-21 18:02:22 +0530

    block-BJaake

--- block-BJaake/code/index.js
+++ block-BJaake/code/index.js
@@ -2,61 +2,57 @@
 
 const url = "https://api.spaceflightnewsapi.net/v3/articles?_limit=30";
 const allContent = document.querySelector(".allcontent");
-const select=document.querySelector("select");
-let specificChannel=[];
+const select = document.querySelector("select");
+let specificChannel = [];
 
-
-function display(sources){
-    sources.forEach(source=>{
-        let option=document.createElement("option");
-        option.innerText=source;
-        option.value=source;
-        select.appendChild(option);
-    })
-    
+function display(sources) {
+  sources.forEach((source) => {
+    let option = document.createElement("option");
+    option.innerText = source;
+    option.value = source;
+    select.appendChild(option);
+  });
 }
 function renderNews(news) {
-    allContent.innerHTML="";
-    news.forEach(element => {
-        let mainClass = document.createElement("div");
-        mainClass.className = "newsContainer";
+  allContent.innerHTML = "";
+  news.forEach((element) => {
+    let mainClass = document.createElement("div");
+    mainClass.className = "newsContainer";
 
-        let img = document.createElement("img");
-        img.src = element.imageUrl;
+    let img = document.createElement("img");
+    img.src = element.imageUrl;
 
-        let div = document.createElement('div');
+    let div = document.createElement("div");
 
-        let span = document.createElement("span");
-        span.innerText = element.newsSite;
+    let span = document.createElement("span");
+    span.innerText = element.newsSite;
 
-        let h3 = document.createElement("h3");
-        h3.innerText = element.title;
+    let h3 = document.createElement("h3");
+    h3.innerText = element.title;
 
-        let a = document.createElement("a");
-        a.href = element.url;
-        a.innerText = "Read More";
+    let a = document.createElement("a");
+    a.href = element.url;
+    a.innerText = "Read More";
 
-        div.append(span, h3, a);
-        mainClass.append(img, div);
+    div.append(span, h3, a);
+    mainClass.append(img, div);
 
-        // Append the dynamically created mainClass to the allContent container
-        allContent.append(mainClass);
-    });
+    // Append the dynamically created mainClass to the allContent container
+    allContent.append(mainClass);
+  });
 }
 
-fetch(url)
-.then((res) => {
-    res.json()
-    .then((data) => {
-        specificChannel=data;
-       renderNews(data);
-       let allNews= [...new Set(data.map((n)=>n.newsSite))];
-       display(allNews);
-    })
-}); 
+fetch(url).then((res) => {
+  res.json().then((data) => {
+    specificChannel = data;
+    renderNews(data);
+    let allNews = [...new Set(data.map((n) => n.newsSite))];
+    display(allNews);
+  });
+});
 
 select.addEventListener("change", (event) => {
-    let source = event.target.value;
-    let filteredNews = specificChannel.filter(data => data.newsSite === source);
-    renderNews(filteredNews);
+  let source = event.target.value;
+  let filteredNews = specificChannel.filter((data) => data.newsSite === source);
+  renderNews(filteredNews);
 });
