abbc8f04b46332cd1c81759e782cea79a60ed7e6
Akhil <akhilkum249@gmail.com>
2024-02-21 18:25:35 +0530

    erro-handling

--- block-BJaajw/code/index.js
+++ block-BJaajw/code/index.js
@@ -1,6 +1,6 @@
 // Corrected version of the code
 
-const url = "https://api.spaceflightnewsapi.net/v3/articles?_limit=30";
+const url = "https://api.spaceflightnewsapi.net/v3/articles?_limit=30/jasbjbjk";
 const allContent = document.querySelector(".allcontent");
 const select = document.querySelector("select");
 let specificChannel = [];
@@ -11,43 +11,51 @@ function display(sources) {
     option.innerText = source;
     option.value = source;
     select.appendChild(option);
-  });
+  });  
 }
 function renderNews(news) {
   allContent.innerHTML = "";
-  news.forEach((element) => {
-    let mainClass = document.createElement("div");
-    mainClass.className = "newsContainer";
-
-    let img = document.createElement("img");
-    img.src = element.imageUrl;
-
-    let div = document.createElement("div");
-
-    let span = document.createElement("span");
-    span.innerText = element.newsSite;
-
-    let h3 = document.createElement("h3");
-    h3.innerText = element.title;
-
-    let a = document.createElement("a");
-    a.href = element.url;
-    a.innerText = "Read More";
-
-    div.append(span, h3, a);
-    mainClass.append(img, div);
-
-    // Append the dynamically created mainClass to the allContent container
-    allContent.append(mainClass);
-  });
-}
+  if(Array.isArray(news)){
+    news.forEach((element) => {
+        let mainClass = document.createElement("div");
+        mainClass.className = "newsContainer";
+    
+        let img = document.createElement("img");
+        img.src = element.imageUrl;
+    
+        let div = document.createElement("div");
+    
+        let span = document.createElement("span");
+        span.innerText = element.newsSite;
+    
+        let h3 = document.createElement("h3");
+        h3.innerText = element.title;
+    
+        let a = document.createElement("a");
+        a.href = element.url;
+        a.innerText = "Read More";
+    
+        div.append(span, h3, a);
+        mainClass.append(img, div);
+    
+       
+        allContent.append(mainClass);
+      });
+    }
+  }
+ 
 
 fetch(url).then((res) => {
+    
   res.json().then((data) => {
+    console.log(data);
     specificChannel = data;
     renderNews(data);
     let allNews = [...new Set(data.map((n) => n.newsSite))];
     display(allNews);
+  })
+  .catch((err)=>{
+    allContent.innerText=err;
   });
 });
 
